f44e65f3cd52c4cc7f6d0df1cdf60bea88caa327
Zahra Mughais <zahramughais@gmail.com>
2022-08-17 19:09:02 +0300

    adding state and birthday method

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,13 +1,25 @@
 import React, {Component} from "react";
 
 class PersonCard extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            age : this.props.age
+        }
+    }
+
+    birthday = () => {
+        this.setState({age : parseInt(this.state.age) + 1 })
+    }
+
     render(){
         const {firstName, lastName, age, hairColor} = this.props
         return (
             <div>
                 <h1>{lastName}, {firstName}</h1>
-                <p>Age: {age}</p>
+                <p>Age: {this.state.age}</p>
                 <p>Hair Color: {hairColor}</p>
+                <button onClick={this.birthday}>Birthday for {firstName} {lastName}</button>
             </div>
         )
     }
